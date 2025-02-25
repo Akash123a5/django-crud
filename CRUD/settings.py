80c8e1cf8c6035e8388a19c4bcf6f686b26e90b9
MD Akash <105543069+Akash123a5@users.noreply.github.com>
2025-02-25 18:53:33 +0600

    Update settings.py

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&miz0qsj9qu$=88#7a_65&-w1!txns)0igeyfkkdue0um#qhqu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-crud-mp1w.onrender.com']
 
 
 # Application definition
